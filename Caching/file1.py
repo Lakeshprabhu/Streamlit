da2b1612ae935f7c5411f7c45c09bc37a9e0ca19
Lakeshprabhu <f20220298@dubai.bits-pilani.ac.in>
2025-09-21 17:19:43 +0400

    Commit Message

---
+++ Caching/file1.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import time
+import numpy as np
+
+from sklearn.linear_model import LinearRegression
+st.write('Caching Test')
+
+st.button('Test Caching')
+
+st.subheader('Cache Data')
+
+@st.cache_data
+def testing():
+    time.sleep(2)
+
+    return "done"
+
+out = testing()
+st.write(out)
+
+
+st.subheader('Cache Resource')
+
+def Linear_model(predo):
+    time.sleep(2)
+    X = np.array([1,2,3,4,5,6,7,8]).reshape(-1,1)
+    Y = np.array([1,2,3,4,5,6,7,8])
+    model = LinearRegression()
+    model = model.fit(X,Y)
+    return
+
